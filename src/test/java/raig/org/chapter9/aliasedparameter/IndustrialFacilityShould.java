0fe67a9e5e88f801bb21e0658777861b6da76c64
oscar-raig <oscar.raig@gmail.com>
2019-03-12 21:31:53 +0100

    Upgrding to gradle 5.0

--- src/test/java/raig/org/chapter9/aliasedparameter/IndustrialFacilityShould.java
+++ src/test/java/raig/org/chapter9/aliasedparameter/IndustrialFacilityShould.java
@@ -3,6 +3,7 @@ package raig.org.chapter9.aliasedparameter;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class IndustrialFacilityShould {
